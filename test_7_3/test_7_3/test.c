7bf1e50b6586a5091293be8bb7309f0036b878aa
qingjiuz <2900105829@qq.com>
2022-07-04 22:33:54 +0800

    learning code

--- test_7_3/test_7_3/test.c
+++ test_7_3/test_7_3/test.c
@@ -2,31 +2,92 @@
 
 #include<stdio.h>
 
+//int main()
+//{
+//	int a = 10;
+//	short b = 0;
+//	printf("%d\n", sizeof(b = a + 5));//2   sizeof()--sizeof函数括号内部不进行运算
+//	printf("%d\n", b); //0
+//
+//	//int a = 0;
+//	//char b = 'b';
+//	//int arr[10] = { 0 };
+//
+//	//printf("%d\n", sizeof(a));// 4
+//	//printf("%d\n", sizeof(int));
+//
+//	//printf("%d\n", sizeof(b));// 1
+//	//printf("%d\n", sizeof(char));
+//
+//	//printf("%d\n", sizeof(arr));//4*10
+//	//printf("%d\n", sizeof(int [10]));
+//
+//	//int  a = 3;
+//	//int* b = &a;//取地址操作符
+//	//*b = 5;//解引用操作符
+//	//printf("%d\n", a);
+//	//printf("%p\n", &b);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 11;//00000000000000000000000000001011
+//	a = a | (1 << 2);//00000000000000000000000000001111
+//	printf("%d\n", a);
+//	
+//	a = a & ~(1 << 2);
+//	printf("%d\n", a);
+//
+//	//int a = 0;
+//	////00000000000000000000000000000000
+//	////11111111111111111111111111111111 补码
+//	////11111111111111111111111111111110 反码
+//	////10000000000000000000000000000001 源码 -1
+//	//printf("%d\n", ~a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	printf("%d\n", a++);//10
+//	printf("%d\n", ++a);//11
+//	return 0;
+//}
+
+//void test1(int arr[])
+//{
+//	printf("%d\n", sizeof(arr));
+//}
+//
+//void test2(char ch[])
+//{
+//	printf("%d\n", sizeof(ch));
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	char ch[10] = { 0 };
+//	printf("%d\n", sizeof(arr));//40
+//	printf("%d\n", sizeof(ch));//10
+//	test1(arr);// 4/8
+//	test2(ch);// 4/8
+//
+//	return 0;
+//}
+
 int main()
 {
-	//int a = 10;
-	//short b = 0;
-	//printf("%d\n", sizeof(b = a + 5));//2   sizeof()--sizeof函数括号内部不进行运算
-	//printf("%d\n", b); //0
+	int a = 11;//1011
+	int b = 6;//0110
+	int c = 0;
+	c = a & b;//0010  --> 2
+	printf("%d\n", c);
 
-	//int a = 0;
-	//char b = 'b';
-	//int arr[10] = { 0 };
-
-	//printf("%d\n", sizeof(a));// 4
-	//printf("%d\n", sizeof(int));
-
-	//printf("%d\n", sizeof(b));// 1
-	//printf("%d\n", sizeof(char));
-
-	//printf("%d\n", sizeof(arr));//4*10
-	//printf("%d\n", sizeof(int [10]));
-
-	int  a = 3;
-	int* b = &a;
-	*b = 5;
-	printf("%d\n", a);
-	printf("%p\n", &b);
-
+	c = a | b;//1111  -->15
+	printf("%d\n", c);
 	return 0;
 }
---
+++ test_7_4/test_7_4/test.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+////创建结构体
+//struct Stu
+//{
+//	char name[20];
+//	int age;
+//	char id[10];
+//};
+//
+//int main()
+//{
+//	//使用结构体创建对象
+//	struct Stu s1 = { "Tony", 20, "20220704" };
+//	struct Stu* p = &s1;
+//	printf("%s\n", p->name);
+//	printf("%d\n", p->age);
+//	printf("%s\n", p->id);
+//
+//	//printf("%s\n", (*p).name);
+//
+//	//printf("%s\n", s1.name);
+//	//printf("%d\n", s1.age);
+//	//printf("%s\n", s1.id);
+//	return 0;
+//}
+
+int main()
+{
+	char a = 3;
+	char b = 127;
+	char c = a + b;
+	peintf("%d\n", c);// -126
+	return 0;
+}
+
+
