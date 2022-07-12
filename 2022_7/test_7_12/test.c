95d6f4c7268c7ff14d5687e47d417883d38c91dc
qingjiuz <2900105829@qq.com>
2022-07-12 23:11:33 +0800

    learning code

---
+++ 2022_7/test_7_12/test.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<string.h>
+#include<assert.h>
+
+
+////unsigned int == size_t
+//size_t my_strlen(const char* arr)
+//{
+//	//assert(arr != NULL);
+//	assert(arr);
+//	size_t count = 0;
+//	while (*arr++)
+//	{
+//		count++;
+//	}
+//	return count;
+//}
+//
+////size_t my_strlen(const char* arr)
+////{
+////	assert(arr);
+////	char* p = arr;
+////	while (*arr++);
+////	return (arr - p - 1);
+////}
+//
+//
+//int main()
+//{
+//	char arr[10] = "hello";
+//	//int n = strlen(arr);
+//	//int n = my_strlen(arr);
+//	printf("%d\n", my_strlen(arr));
+//	return 0;
+//}
+
+
+
