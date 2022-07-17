7df1d74405403d3e2acb2b690ee57bcc807ba114
qingjiuz <2900105829@qq.com>
2022-07-17 23:42:08 +0800

    learning code

---
+++ 2022_7/test_7_17/test.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//    int a = 0;
+//    while (scanf("%d", &a) != EOF)
+//    {
+//        printf("%d\n", a);
+//    }
+//    return 0;
+//}
+
+
+//typedef struct N
+//{
+//	int data;
+//	struct N* next;
+//}N;
+
+
+//int main()
+//{
+//	//register 起一个建议的作用
+//	register int num = 10;
+//
+//	return 0;
+//}
+
+//extern int g_val;
+//extern int Add(int x, int y);
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int sum = a + b;//Add(a, b);
+	printf("%d\n", sum);
+	return 0;
+}
