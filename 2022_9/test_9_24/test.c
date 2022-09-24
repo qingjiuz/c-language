436d2af532e8fd95aebe69c6cd7eb9ec8aa9501e
qingjiuz <2900105829@qq.com>
2022-09-24 22:07:15 +0800

    learning code

---
+++ 2022_9/test_9_24/test.c
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+////BC76 公务员面试
+
+//int main() {
+//    int num = 0;
+//    int max = 0;
+//    int min = 100;
+//    int sum = 0;
+//    int count = 0;
+//    while (scanf("%d", &num) != EOF) { 
+//        if (num > max) {
+//            max = num;
+//        }
+//        if (num < min) {
+//            min = num;
+//        }
+//        sum += num;
+//        count++;
+//        if (count == 7) {
+//            printf("%.2f\n", (sum - min - max) / 5.0);
+//            max = 0;
+//            min = 100;
+//            sum = 0;
+//            count = 0;
+//        }
+//    }
+//
+//    return 0;
+//}
+//
+
+//BC74 HTTP状态码
+//int main() {
+//    int a = 0;
+//    while (scanf("%d", &a) != EOF) { 
+//        switch (a) {
+//        case 200:
+//            printf("OK\n");
+//            break;
+//        case 202:
+//            printf("Accepted\n");
+//            break;
+//        case 400:
+//            printf("Bad Request\n");
+//            break;
+//
+//        case 403:
+//            printf("Forbidden\n");
+//            break;
+//
+//        case 404:
+//            printf("Not Found\n");
+//            break;
+//
+//        case 500:
+//            printf("Internal Server Error\n");
+//            break;
+//
+//        case 502:
+//            printf("Bad Gateway\n");
+//            break;
+//        }
+//    }
+//    return 0;
+//}
+
