780046644ed164b08d9fe560f869b74d259d6f6a
qingjiuz <2900105829@qq.com>
2022-10-23 21:06:02 +0800

    通讯录（静态版本）

---
+++ Contact/Contact/test.c
@@ -0,0 +1,65 @@
+#include "contact.h"
+
+
+void mune()
+{
+	printf("************************************\n");
+	printf("*****  1. Add       2. Delete  *****\n");
+	printf("*****  3. Search    4. Modify  *****\n");
+	printf("*****  5. Show      6. Remove  *****\n");
+	printf("*****  7. Sort      0. Exit    *****\n");
+	printf("************************************\n");
+}
+
+void test()
+{
+	int input = 0;
+	//创建通讯录
+	struct Contact con;
+	//初始化通讯录
+	InitContact(&con);
+	do
+	{
+		mune();
+		printf("请输入:> ");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1://add
+			AddContact(&con);
+			break;
+		case 2://delete
+			DeleteContact(&con);
+			break;
+		case 3://search
+			SearchContact(&con);
+			break;
+		case 4://modify
+			ModifyContact(&con);
+			break;
+		case 5://show
+			ShowContact(&con);
+			break;
+		case 6://remove
+			RemoveContant(&con);
+			break;
+		case 7://sort
+			SortContact(&con);
+			break;
+		case 0:
+			printf("退出通讯录\n");
+			break;
+		default:
+			printf("请重新输入正确的数字\n");
+			break;
+		}
+
+	} while (input);
+
+}
+
+int main()
+{
+	test();
+	return 0;
+}
