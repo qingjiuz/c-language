321f3c6bec3ec2cc7e7106157cd5c2ffa94632b1
qingjiuz <2900105829@qq.com>
2022-10-24 14:46:13 +0800

    learning code

---
+++ 2022_10/test_10_24/test.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+////BC118-小乐乐与序列
+//int main() {
+//    int n = 0;
+//    scanf("%d", &n);
+//    int arr[100000] = { 0 };
+//    int max = 0;
+//    int i = 0;
+//    for (i = 0; i < n; i++) {
+//        int tmp = 0;
+//        scanf("%d", &tmp);
+//        arr[tmp] = tmp;
+//    }
+//
+//    for (i = 0; i < n; i++)
+//    {
+//        if (arr[i])
+//        {
+//            printf("%d ", arr[i]);
+//        }
+//    }
+//
+//
+//    return 0;
+//}
+
+////BC60 带空格直角三角形图案
+//int main() {
+//    int a = 0;
+//    while (scanf("%d", &a) != EOF) {
+//        int i = 0;
+//        for (i = 0; i < a; i++)
+//        {
+//            int j = 0;
+//            for (j = 0; j < a - i - 1; j++)
+//            {
+//                printf("  ");
+//            }
+//            for (j = 0; j < i + 1; j++)
+//            {
+//                printf("* ");
+//            }
+//            printf("\n");
+//        }
+//    }
+//    return 0;
+//}
+
